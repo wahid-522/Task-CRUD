2c60c075341e0748e68223b0777ddd731d7e07ff
Wahid Rehman <wahid72701@gmail.com>
2025-08-13 15:59:56 +0500

    inital

--- lib/views/get_all_priority.dart
+++ lib/views/get_all_priority.dart
@@ -11,6 +11,7 @@ class GetAllPrioritiesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(title: Text("Get All Priorities")),
       floatingActionButton: FloatingActionButton(
